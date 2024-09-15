3a7c9aabf1069a7bb4218aa0ed41d6e60de7342a
Laura Antunez <lauraantunez024@gmail.com>
2024-09-14 22:43:27 -0400

    add nextjs front end

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017')
+db = client['movie_tracker']
+movies_collection = db['movies']
+
+
+@app.route('/movies', methods=['GET'])
+def getMovies():
+    movies = list(movies_collection.find({}, {'_id':0}))
+    return jsonify(movies)
+
+@app.route('/movies/watched', methods=['POST'])
+def markAsWatched():
+    title = request.json.get('title')
+    movies_collection.update_one({"title": title}, {"$set": {"watched": True}})
+    return jsonify({"message": f"Marked '{title}' as watched"})
+
+@app.route('/movies/rate', methods=['POST'])
+def rate_movie():
+    title = request.json.get('title')
+    rating = request.json.get('rating')
+    comments = request.json.get('comments')
+    movies_collection.update_one({"title": title}, {"$set": {"rating": rating, "comments": comments}})
+    return jsonify({"message": f"Updated '{title}' with rating and comments"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
