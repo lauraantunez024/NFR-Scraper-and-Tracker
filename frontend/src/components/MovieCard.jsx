46fbfd7594bb67796579622513546f5a666d7d80
Laura Antunez <lauraantunez024@gmail.com>
2024-09-26 18:13:30 -0400

    improved card grid format

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,16 +1,14 @@
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
 
-require('dotenv').config({
-
-})
+require("dotenv").config({});
 
 export default function MovieCard({ title, imdb_rating, year, movie_data }) {
   const [isLoading, setIsLoading] = useState([false]);
   const [movieData, setMovieData] = useState([]);
   const [posterData, setPosterData] = useState([]);
   const [imagePath, setImagePath] = useState([]);
-  const api_key = process.env.NEXT_PUBLIC_TMDB_API_KEY
+  const api_key = process.env.NEXT_PUBLIC_TMDB_API_KEY;
   // const api_key = process.env.TMDB_API_KEY;
   const movieUrl = `https://api.themoviedb.org/3/find/${movie_data.imDB_ID}?external_source=imdb_id&api_key=${api_key}`;
 
@@ -29,7 +27,7 @@ export default function MovieCard({ title, imdb_rating, year, movie_data }) {
         const posterData = await posterDataResponse.json();
         setPosterData(posterData);
 
-        const image_link = `https://image.tmdb.org/t/p/w500${posterData.posters[0].file_path}`
+        const image_link = `https://image.tmdb.org/t/p/w500${posterData.posters[0].file_path}`;
         setImagePath(image_link);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -43,8 +41,8 @@ export default function MovieCard({ title, imdb_rating, year, movie_data }) {
     <div class={styles.card}>
       <h1 class={styles.title}>{title}</h1>
 
+      <img class={styles.card_image} src={imagePath} alt="womp" />
       <div class={styles.card_details}>
-        <img class={styles.card_image} src={imagePath} alt="womp" />
         <p>imdb rating: {imdb_rating}</p>
         <p>year: {year}</p>
       </div>
