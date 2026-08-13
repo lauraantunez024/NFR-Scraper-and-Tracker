a562ca085fafa62e489ce5b62bd521bb6d44d5ce
Laura Antunez <lauraantunez024@gmail.com>
2026-08-12 20:31:56 -0400

    added click on mobile

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -8,6 +8,7 @@ export default function MovieCard({ title, imdb_rating, year, movie_data }) {
   const [posterData, setPosterData] = useState([]);
   const [imagePath, setImagePath] = useState([]);
   const [logoPath, setLogoPath] = useState([]);
+  const [showDetails, setShowDetails] = useState(false);
   const api_key = process.env.NEXT_PUBLIC_TMDB_API_KEY;
   // const api_key = process.env.TMDB_API_KEY;
   const movieUrl = `https://api.themoviedb.org/3/find/${movie_data.imDB_ID}?external_source=imdb_id&api_key=${api_key}`;
@@ -43,16 +44,22 @@ export default function MovieCard({ title, imdb_rating, year, movie_data }) {
     fetchTmDBData();
   }, []);
   return (
-    <div className="min-w-[10vw] p-2 h-[40vh] bg-[#6776b5] lg:group-hover:bg-[#6776b5]/50 justify-around text-(--primary) rounded-md border-[#141414] border-x-4 border-y-2 border flex flex-col gap-4">
+    <div className="min-w-[10vw] p-2 h-[40vh] bg-[#6776b5] lg:group-hover:bg-[#6776b5]/50 justify-around text-(--primary) rounded-md border-[#141414] border-x-4 border-y-2 border flex flex-col gap-4"
+    onClick={() => setShowDetails((prev) => !prev)} tabIndex={0}>
 
       {logoPath ? (
-        <img className="p-4 flex text-center shrink text-white lg:group-hover:hidden group-focus:hidden max-h-[30vh] lg:max-w-[18vw] m-auto text-2xl" onerror="this.style.display='none'" src={logoPath} alt={title} />
+        <img className={`p-4 flex text-center shrink text-white lg:group-hover:hidden group-focus:hidden max-h-[30vh] lg:max-w-[18vw] m-auto text-2xl ${showDetails ? "max-lg:hidden" : ""}`} onerror="this.style.display='none'" src={logoPath} alt={title} />
       ) : (
-        <h2 className="text-center text-2xl text-white lg:group-hover:hidden">{title}</h2>
+        <h2 className={`text-center text-2xl text-white lg:group-hover:hidden ${showDetails ? "max-lg:hidden" : ""}`}>{title}</h2>
 
       )}
-      <div className="hidden group-hover:block flex flex-col justify-evenly text-white">
-
+<div
+  className={`
+    hidden flex-col justify-evenly text-white
+    ${showDetails ? "max-lg:flex" : ""}
+    lg:group-hover:flex
+  `}
+>
         <h1 className="text-center text-lg font-bold">{title}</h1>
         {/* <img className="p-4 flex shrink" src={logoPath} alt={title} /> */}
         <div className="flex flex-row gap-4 m-auto">
