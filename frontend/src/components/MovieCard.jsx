10ce9a9e20a0b544a02f274e441d0753b351f5a5
Laura Antunez <lauraantunez024@gmail.com>
2026-08-12 20:15:18 -0400

    improved logo size

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -43,12 +43,12 @@ export default function MovieCard({ title, imdb_rating, year, movie_data }) {
     fetchTmDBData();
   }, []);
   return (
-    <div className="min-w-[10vw] p-2 h-[40vh] bg-[#6776b5] group-hover:bg-[#6776b5]/50 justify-around text-(--primary) rounded-md border-[#141414] border-x-4 border-y-2 border flex flex-col gap-4">
+    <div className="min-w-[10vw] p-2 h-[40vh] bg-[#6776b5] lg:group-hover:bg-[#6776b5]/50 justify-around text-(--primary) rounded-md border-[#141414] border-x-4 border-y-2 border flex flex-col gap-4">
 
       {logoPath ? (
-        <img className="p-4 flex text-center shrink text-white group-hover:hidden max-h-[30vh] max-w-[18vw] m-auto text-2xl" onerror="this.style.display='none'" src={logoPath} alt={title} />
+        <img className="p-4 flex text-center shrink text-white lg:group-hover:hidden group-focus:hidden max-h-[30vh] lg:max-w-[18vw] m-auto text-2xl" onerror="this.style.display='none'" src={logoPath} alt={title} />
       ) : (
-        <h2 className="text-center text-2xl text-white group-hover:hidden">{title}</h2>
+        <h2 className="text-center text-2xl text-white lg:group-hover:hidden">{title}</h2>
 
       )}
       <div className="hidden group-hover:block flex flex-col justify-evenly text-white">
