b87898cf1452c3f2dc9fc62356e45563ec6146d4
Laura Antunez <lauraantunez024@gmail.com>
2026-08-25 16:22:32 -0400

    fixed NaN currency bug

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -8,17 +8,23 @@ export default function MovieCard({ title, imdb_rating, year, movie_data }) {
   const [showModal, setShowModal] = useState(false);
 
   function formatted_currency(amount) {
-    amount = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD"
-    }).format(amount)
-    return amount
+    if (!amount) {
+      return "N/A"
+
+    } else {
+
+      amount = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+      }).format(amount)
+      return amount
+    }
   }
 
   const logoPath = movie_data?.LogoImage;
   return (
     <div className="min-w-[10vw] p-2 h-[30vh] max-h-[15rem] bg-[#6776b5] lg:group-hover:bg-[#6776b5]/50 justify-around text-(--primary) rounded-md border-[#141414] border-x-4 border-y-2 border flex flex-col gap-4"
-    onClick={() => setShowDetails((prev) => !prev)} tabIndex={0}>
+      onClick={() => setShowDetails((prev) => !prev)} tabIndex={0}>
 
       {logoPath ? (
         <img className={`p-4 flex text-center shrink text-white lg:group-hover:hidden group-focus:hidden max-h-[10rem] lg:max-w-[18vw] m-auto text-2xl ${showDetails ? "max-lg:hidden" : ""}`} onerror="this.style.display='none'" src={logoPath} alt={title} />
@@ -26,13 +32,13 @@ export default function MovieCard({ title, imdb_rating, year, movie_data }) {
         <h2 className={`text-center text-2xl text-white lg:group-hover:hidden ${showDetails ? "max-lg:hidden" : ""}`}>{title}</h2>
 
       )}
-<div
-  className={`
+      <div
+        className={`
     hidden flex-col h-full justify-between p-2 text-white
     ${showDetails ? "max-lg:flex" : ""}
     lg:group-hover:flex
   `}
->
+      >
         <h1 className="text-center text-xl font-bold">{title}</h1>
         {/* <img className="p-4 flex shrink" src={logoPath} alt={title} /> */}
         <div className="flex flex-col w-full text-center justify-around">
@@ -40,20 +46,20 @@ export default function MovieCard({ title, imdb_rating, year, movie_data }) {
           <p>year inducted: {movie_data?.yearInducted}</p>
 
         </div>
-          <button type="button" onClick={(e) => { e.stopPropagation(); setShowModal(true)}} className="underline"> More Info </button>
+        <button type="button" onClick={(e) => { e.stopPropagation(); setShowModal(true) }} className="underline"> More Info </button>
       </div>
 
-          <MovieModal 
-          title={title}
-          open={showModal}
-          year={year}
-          imdb_rating={imdb_rating}
-          plot={movie_data?.plot}
-          tagline={movie_data?.tagline}
-          budget={formatted_currency(movie_data?.budget)}
-          revenue={formatted_currency(movie_data?.revenue)}
-          poster={movie_data?.posterImage}
-          onClose={() => setShowModal(false)}/>
+      <MovieModal
+        title={title}
+        open={showModal}
+        year={year}
+        imdb_rating={imdb_rating}
+        plot={movie_data?.plot}
+        tagline={movie_data?.tagline}
+        budget={formatted_currency(movie_data?.budget)}
+        revenue={formatted_currency(movie_data?.revenue)}
+        poster={movie_data?.posterImage}
+        onClose={() => setShowModal(false)} />
     </div>
   );
 }
